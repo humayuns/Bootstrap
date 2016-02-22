aa5733f1560bfc50e53588d93fdd7ec38b2442c0
Humayun Shabbir <humayun_shabbir@hotmail.com>
2016-02-22 09:13:26 +0500

    Added model, view and controller for Snippets.

---
+++ Bootstrap/Models/BootstrapContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Bootstrap.Models
+{
+    public class BootstrapContext : DbContext
+    {
+        public BootstrapContext() : base("DefaultConnection")
+        {
+        }
+        public DbSet<Snippet> Snippets { get; set; } 
+
+    }
+}
---
+++ Bootstrap/Models/Snippet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bootstrap.Models
+{
+    public class Snippet
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        [System.Web.Mvc.AllowHtml]
+        public string Content { get; set; }
+    }
+}
